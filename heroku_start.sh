2fd0a7adb84499e5ea74e9e69557cac4175e6ad3
008 <104703008@nccu.edu.tw>
2018-11-08 23:10:27 +0800

    add heroku settings

---
+++ heroku_start.sh
@@ -0,0 +1,13 @@
+
+bash db_clean_up.sh
+find . -type d -name __pycache__ -exec rm -r {} \+
+
+python3 manage.py makemigrations
+python3 manage.py migrate --run-syncdb
+python3 gen_fixtures.py st 3
+python3 ./manage.py loaddata output.json
+python3 ./manage.py shell < set_password.py
+
+python3 ./manage.py runserver 0.0.0.0:$PORT
+
+
